4a41468c1332f3555a7e5bcd0894f8c5704b1d69
czx-xream <953934518@qq.com>
2024-11-17 19:35:06 +0800

    初始框架

---
+++ app/src/main/java/com/example/tikmusic/MainActivity.java
@@ -0,0 +1,47 @@
+package com.example.tikmusic;
+
+import android.os.Bundle;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // 默认加载 ExploreFragment
+        getSupportFragmentManager().beginTransaction()
+                .replace(R.id.fragment_container, new ExploreFragment())
+                .commit();
+
+        // 底部导航栏逻辑
+        BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_navigation);
+        bottomNavigationView.setOnNavigationItemSelectedListener(item -> {
+            Fragment selectedFragment = null;
+
+            if (item.getItemId() == R.id.nav_explore) {
+                //item.setIcon(R.drawable.ic_headphone_select);
+                selectedFragment = new ExploreFragment();
+            } else if (item.getItemId() == R.id.nav_playmusic) {
+                //item.setIcon(R.drawable.ic_music_play_select);
+                selectedFragment = new PlayMusicFragment();
+            } else if (item.getItemId() == R.id.nav_userinfo) {
+                //item.setIcon(R.drawable.ic_user_select);
+                selectedFragment = new UserInfoFragment();
+            }
+
+            if (selectedFragment != null) {
+                getSupportFragmentManager().beginTransaction()
+                        .replace(R.id.fragment_container, selectedFragment)
+                        .commit();
+            }
+            return true;
+        });
+
+    }
+}
